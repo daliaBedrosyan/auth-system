e1524a053f80ab35a6a9b98c57311ca199dbe071
dalia9999 <daliabedrosyan47@gmail.com>
2025-05-20 19:22:39 +0300

    Implement: user signup logic with validation, password hashing, and JWT cookie

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,6 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!!');
-});
-
 app.use('/api/auth', authRoutes)
 
 
