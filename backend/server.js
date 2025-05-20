99b21f36a5ba0f86b71c8e1ba6257fae2fb4749e
dalia9999 <daliabedrosyan47@gmail.com>
2025-05-20 16:57:29 +0300

    Add: Mongoose schema and model for User

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,21 @@ dotenv.config();
 
 import express from 'express';
 import { connectDB } from './db/connectDB.js';
+import { authRoutes } from './routes/auth.route.js';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!!');
 });
 
+app.use('/api/auth', authRoutes)
+
 
 app.listen(PORT, () => {
     connectDB();
-    console.log('Server running on http://localhost:3000');
+    console.log('Server running on http://localhost:' + PORT);
 })
